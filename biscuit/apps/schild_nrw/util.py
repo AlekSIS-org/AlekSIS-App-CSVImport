e9213344917bf2268dc56009f16b90904ae7ff7c
Dominik George <dominik.george@teckids.org>
2019-07-15 17:39:52 +0200

    Decode CSV file.

--- biscuit/apps/schild_nrw/util.py
+++ biscuit/apps/schild_nrw/util.py
@@ -1,11 +1,13 @@
 from biscuit.core.models import Person
 
+import codecs
 import csv
 
 
 def schild_import_csv(teachers_csv, students_csv, guardians_csv):
-    teachers_reader = csv.DictReader(teachers_csv, fieldnames=('guid', 'email', 'email_business', 'date_of_birth',
-                                                               'sex', 'abbrev', 'last_name', 'first_name', 'street', 'postal_code', 'place', 'phone_number', 'mobile_number', 'visible'))
+    teachers_reader = csv.DictReader(codecs.iterdecode(teachers_csv, 'utf-8'),
+                                     fieldnames=('guid', 'email', 'email_business', 'date_of_birth',
+                                                 'sex', 'abbrev', 'last_name', 'first_name', 'street', 'postal_code', 'place', 'phone_number', 'mobile_number', 'visible'))
 
     for teacher_row in teachers_reader:
         if teacher_row['visible']:
