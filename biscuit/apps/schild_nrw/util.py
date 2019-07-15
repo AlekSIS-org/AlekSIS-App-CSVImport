85deb5e0f45b0fe71c1c602cb3668dee9aabde3c
Dominik George <dominik.george@teckids.org>
2019-07-15 17:17:39 +0200

    Import teachers from CSV.

--- biscuit/apps/schild_nrw/util.py
+++ biscuit/apps/schild_nrw/util.py
@@ -1,2 +1,13 @@
+from biscuit.core.models import Person
+
+import csv
+
+
 def schild_import_csv(teachers_csv, students_csv, guardians_csv):
-    pass
+    teachers_reader = csv.DictReader(teachers_csv, fieldnames=('guid', 'email', 'email_business', 'date_of_birth',
+                                                               'sex', 'abbrev', 'last_name', 'first_name', 'street', 'postal_code', 'place', 'phone_number', 'mobile_number', 'visible'))
+
+    for teacher_row in teachers_reader:
+        if teacher_row['visible']:
+            person, created = Person.objects.get_or_create(
+                guid=teacher_row['guid'], defaults=teacher_row)
