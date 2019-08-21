091785b0673f501a4868eb05501a63f499519a9a
Dominik George <nik@naturalnet.de>
2019-08-21 23:53:06 +0200

    Use ugettext everywhere. Advances BiscuIT-ng#38.

--- biscuit/apps/schild_nrw/management/commands/schild_import.py
+++ biscuit/apps/schild_nrw/management/commands/schild_import.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.utils.translation import gettext as _
+from django.utils.translation import ugettext as _
 
 from biscuit.apps.schild_nrw.util import schild_import_csv
 
