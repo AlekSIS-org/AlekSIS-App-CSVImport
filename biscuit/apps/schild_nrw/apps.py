785efa7614ffb692fdbd4b18d196f28c293a7d28
Dominik George <nik@naturalnet.de>
2019-09-05 22:33:28 +0200

    Fix name translation.

--- biscuit/apps/schild_nrw/apps.py
+++ biscuit/apps/schild_nrw/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext as _
 
 
 class SchILDNRWConfig(AppConfig):
     name = 'biscuit.apps.schild_nrw'
-    verbose_name = 'BiscuIT - ' + _('SchILD-NRW interface')
+    verbose_name = 'BiscuIT - SchILD-NRW interface'
