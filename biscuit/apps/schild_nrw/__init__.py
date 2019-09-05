e88284738dc039be8c23c2a55429054677808f36
Dominik George <nik@naturalnet.de>
2019-09-05 22:07:31 +0200

    Update app name and version in module.

--- biscuit/apps/schild_nrw/__init__.py
+++ biscuit/apps/schild_nrw/__init__.py
@@ -1,1 +1,8 @@
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution('BiscuIT-App-SchILD-NRW').version
+except Exception:
+    __version__ = 'unknown'
+
 default_app_config = 'biscuit.apps.schild_nrw.apps.SchILDNRWConfig'
--- biscuit/apps/schild_nrw/apps.py
+++ biscuit/apps/schild_nrw/apps.py
@@ -4,4 +4,4 @@ from django.utils.translation import ugettext_lazy as _
 
 class SchILDNRWConfig(AppConfig):
     name = 'biscuit.apps.schild_nrw'
-    verbose_name = _('BiscuIT - ' + 'SchILD-NRW interface')
+    verbose_name = 'BiscuIT - ' + _('SchILD-NRW interface')
