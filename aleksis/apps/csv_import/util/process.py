8d526d0ac027b5c52c9d319ceb20d1cc0694f2ca
Jonathan Weth <git@jonathanweth.de>
2020-05-12 20:32:53 +0200

    Move import code to one function and define some constants

--- aleksis/apps/csv_import/util/process.py
+++ aleksis/apps/csv_import/util/process.py
@@ -11,97 +11,19 @@ from aleksis.apps.csv_import.models import ImportTemplate
 from aleksis.core.models import Person
 from aleksis.core.util import messages
 
-SCHILD_STATE_ACTIVE = (True, 2)
+STATE_ACTIVE = (True, 2)
+TRUE_VALUES = ["+", "Ja"]
+FALSE_VALUES = ["-", "Nein"]
+DATE_FIELDS = ["date_of_birth"]
 
 
-def is_active(person_row: dict) -> bool:
-    """ Find out whether an imported person is active by looking
-    at different attributes.
-    """
+def is_active(row: dict) -> bool:
+    """Find out whether an imported object is active."""
 
-    if "is_active" in person_row:
-        return person_row["is_active"] in SCHILD_STATE_ACTIVE
+    if "is_active" in row:
+        return row["is_active"] in STATE_ACTIVE
 
     return True
-
-
-def import_csv_single(
-    request: HttpRequest,
-    csv: Union[BinaryIO, str],
-    cols: Dict[str, Any],
-    converters: Dict[str, Callable[[Optional[str]], Any]],
-) -> None:
-    persons = pandas.read_csv(
-        csv,
-        sep=";",
-        names=cols.keys(),
-        dtype=cols,
-        usecols=lambda k: not k.startswith("_"),
-        keep_default_na=False,
-        converters=converters,
-        parse_dates=["date_of_birth"],
-        quotechar='"',
-        encoding="utf-8-sig",
-        true_values=["+", "Ja"],
-        false_values=["-", "Nein"],
-    )
-
-    # Clean up invalid date values
-    persons.date_of_birth = persons.date_of_birth.astype(object)
-    persons = persons.where(persons.notnull(), None)
-
-    all_ok = True
-    inactive_refs = []
-    created_count = 0
-
-    for person_row in persons.transpose().to_dict().values():
-        # Fill the is_active field from other fields if necessary
-        person_row["is_active"] = is_active(person_row)
-
-        if person_row["is_active"]:
-            try:
-                person, created = Person.objects.update_or_create(
-                    import_ref=person_row["import_ref"], defaults=person_row
-                )
-            except (ValueError, phonenumbers.NumberParseException) as err:
-                messages.error(
-                    request,
-                    _(
-                        "Failed to import person %s"
-                        % ("%s, %s" % (person_row["last_name"], person_row["first_name"]))
-                    )
-                    + ": %s" % err,
-                    fail_silently=True,
-                )
-                all_ok = False
-
-            # Ensure that newly set primary group is also in member_of
-            if person.primary_group and person.primary_group not in person.member_of.all():
-                person.member_of.add(person.primary_group)
-                person.save()
-
-            if created:
-                created_count += 1
-        else:
-            # Store import refs to deactivate later
-            inactive_refs.append(person_row["import_ref"])
-
-    # Deactivate all persons that existed but are now inactive
-    if inactive_refs:
-        affected = Person.objects.filter(import_ref__in=inactive_refs, is_active=True).update(
-            is_active=False
-        )
-
-        if affected:
-            messages.warning(request, _("%d existing persons were deactivated.") % affected)
-
-    if created_count:
-        messages.success(request, _("%d persons were newly created.") % created_count)
-
-    if all_ok:
-        messages.success(request, _("All persons were imported successfully."))
-    else:
-        messages.warning(request, _("Some persons failed to be imported."))
 
 
 def import_csv(
@@ -134,8 +56,6 @@ def import_csv(
         ]
     )
 
-    import_csv_single(request, csv, teachers_csv_cols, csv_converters)
-
     students_csv_cols = OrderedDict(
         [
             ("import_ref", str),
@@ -157,4 +77,87 @@ def import_csv(
         ]
     )
 
-    import_csv_single(request, csv, students_csv_cols, csv_converters)
+    cols = students_csv_cols
+
+    data = pandas.read_csv(
+        csv,
+        sep=";",
+        names=cols.keys(),
+        dtype=cols,
+        usecols=lambda k: not k.startswith("_"),
+        keep_default_na=False,
+        converters=csv_converters,
+        parse_dates=DATE_FIELDS,
+        quotechar='"',
+        encoding="utf-8-sig",
+        true_values=TRUE_VALUES,
+        false_values=FALSE_VALUES,
+    )
+
+    # Clean up invalid date values
+    data.date_of_birth = data.date_of_birth.astype(object)
+
+    # Exclude all empty rows
+    data = data.where(data.notnull(), None)
+
+    all_ok = True
+    inactive_refs = []
+    created_count = 0
+
+    for person_row in data.transpose().to_dict().values():
+        # Fill the is_active field from other fields if necessary
+        person_row["is_active"] = is_active(person_row)
+
+        if person_row["is_active"]:
+            created = False
+            try:
+                person, created = Person.objects.update_or_create(
+                    import_ref=person_row["import_ref"], defaults=person_row
+                )
+            except (ValueError, phonenumbers.NumberParseException) as err:
+                messages.error(
+                    request,
+                    _(
+                        "Failed to import person %s"
+                        % (
+                            "%s, %s"
+                            % (person_row["last_name"], person_row["first_name"])
+                        )
+                    )
+                    + ": %s" % err,
+                    fail_silently=True,
+                )
+                all_ok = False
+
+            # Ensure that newly set primary group is also in member_of
+            if (
+                person.primary_group
+                and person.primary_group not in person.member_of.all()
+            ):
+                person.member_of.add(person.primary_group)
+                person.save()
+
+            if created:
+                created_count += 1
+        else:
+            # Store import refs to deactivate later
+            inactive_refs.append(person_row["import_ref"])
+
+    # Deactivate all persons that existed but are now inactive
+    if inactive_refs:
+        affected = Person.objects.filter(
+            import_ref_csv__in=inactive_refs, is_active=True
+        ).update(is_active=False)
+
+        if affected:
+            messages.warning(
+                request, _("%d existing persons were deactivated.") % affected
+            )
+
+    if created_count:
+        messages.success(request, _("%d persons were newly created.") % created_count)
+
+    if all_ok:
+        messages.success(request, _("All persons were imported successfully."))
+    else:
+        messages.warning(request, _("Some persons failed to be imported."))
