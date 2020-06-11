30d0d43a2ebb3300c0c72438473f8fb7c3038b29
Jonathan Weth <git@jonathanweth.de>
2020-06-11 16:27:40 +0200

    Fix connection of subjects to department groups (update or create GroupSubject)

--- aleksis/apps/csv_import/util/import_helpers.py
+++ aleksis/apps/csv_import/util/import_helpers.py
@@ -2,7 +2,7 @@ from typing import List, Optional, Sequence, Union
 
 from django.db.models import Model
 
-from aleksis.apps.chronos.models import Subject
+from aleksis.apps.chronos.models import GroupSubject, Subject
 from aleksis.apps.csv_import.models import ALLOWED_FIELD_TYPES_FOR_MODELS, FieldType
 from aleksis.apps.csv_import.settings import STATE_ACTIVE
 from aleksis.core.models import Group, Person
@@ -59,11 +59,12 @@ def create_department_groups(subjects: Sequence[str]) -> Sequence[Group]:
         group, __ = Group.objects.get_or_create(
             group_type=group_type,
             short_name=subject.short_name,
-            defaults={
-                "subject__subject": subject,
-                "name": with_prefix(group_prefix, subject.name),
-            },
+            defaults={"name": with_prefix(group_prefix, subject.name),},
         )
+        GroupSubject.objects.update_or_create(
+            group=group, defaults={"subject": subject}
+        )
+
         groups.append(group)
     return groups
 
