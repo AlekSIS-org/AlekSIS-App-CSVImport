9476781d9fe9a46766b6579b5e18afd130158bc4
Jonathan Weth <git@jonathanweth.de>
2020-06-11 10:14:52 +0200

    Add template for students from Pedasos

--- aleksis/apps/csv_import/templates.py
+++ aleksis/apps/csv_import/templates.py
@@ -70,3 +70,44 @@ def update_or_create_default_templates():
             FieldType.GROUP_OWNER_BY_SHORT_NAME,
         ],
     )
+    update_or_create_template(
+        Person,
+        name="pedasos_students",
+        verbose_name=_("Pedasos: Students"),
+        extra_args={"has_header_row": True, "separator": "\t"},
+        fields=[
+            FieldType.UNIQUE_REFERENCE,
+            FieldType.LAST_NAME,
+            FieldType.FIRST_NAME,
+            FieldType.DATE_OF_BIRTH_DD_MM_YYYY,
+            FieldType.SEX,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GUARDIAN_LAST_NAME,  # Mother
+            FieldType.GUARDIAN_FIRST_NAME,
+            FieldType.GUARDIAN_EMAIL,
+            FieldType.GUARDIAN_LAST_NAME,  # Father
+            FieldType.GUARDIAN_FIRST_NAME,
+            FieldType.GUARDIAN_EMAIL,
+            FieldType.GROUP_BY_SHORT_NAME,  # Course 1
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,  # Course 5
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,  # Course 10
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,  # Course 15
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,
+            FieldType.GROUP_BY_SHORT_NAME,  # Course 20
+            FieldType.GROUP_BY_SHORT_NAME,  # Course 21
+        ],
+    )
