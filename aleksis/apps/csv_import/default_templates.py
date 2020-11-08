84327d68c02da70cee0830e5972a22bf00e992c6
Dominik George <dominik.george@teckids.org>
2020-11-08 16:33:04 +0100

    Move default templates to data file

--- aleksis/apps/csv_import/default_templates.py
+++ aleksis/apps/csv_import/default_templates.py
@@ -1,29 +1,13 @@
 from typing import Sequence
 
+import toml
+
+from django.apps import apps
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Model
 from django.utils.translation import gettext as _
 
-from aleksis.core.models import Group, Person
-
-from .field_types import (
-    ChildByUniqueReference,
-    DateOfBirthFieldType,
-    DepartmentsFieldType,
-    EmailFieldType,
-    FieldType,
-    FirstNameFieldType,
-    GroupMembershipByShortNameFieldType,
-    GroupOwnerByShortNameFieldType,
-    GroupSubjectByShortNameFieldType,
-    IgnoreFieldType,
-    LastNameFieldType,
-    PedasosClassRangeFieldType,
-    PrimaryGroupByShortNameFieldType,
-    SexFieldType,
-    ShortNameFieldType,
-    UniqueReferenceFieldType,
-)
+from .field_types import field_type_registry
 from .models import ImportTemplate, ImportTemplateField
 
 
@@ -51,164 +35,16 @@ def update_or_create_template(
 
 def update_or_create_default_templates():
     """Update or create default import templates."""
-    update_or_create_template(
-        Person,
-        name="pedasos_teachers",
-        verbose_name=_("Pedasos: Teachers"),
-        extra_args={"has_header_row": True, "separator": "\t"},
-        fields=[
-            ShortNameFieldType,
-            LastNameFieldType,
-            FirstNameFieldType,
-            DateOfBirthFieldType,
-            SexFieldType,
-            DepartmentsFieldType,
-            IgnoreFieldType,
-        ],
-    )
-    update_or_create_template(
-        Group,
-        name="pedasos_classes",
-        verbose_name=_("Pedasos: Classes"),
-        extra_args={"has_header_row": True, "separator": "\t"},
-        fields=[
-            ShortNameFieldType,
-            GroupOwnerByShortNameFieldType,
-            GroupOwnerByShortNameFieldType,
-        ],
-    )
-    update_or_create_template(
-        Group,
-        name="pedasos_courses",
-        verbose_name=_("Pedasos: Courses"),
-        extra_args={"has_header_row": True, "separator": "\t"},
-        fields=[
-            ShortNameFieldType,
-            PedasosClassRangeFieldType,
-            GroupSubjectByShortNameFieldType,
-            GroupOwnerByShortNameFieldType,
-        ],
-    )
-    update_or_create_template(
-        Person,
-        name="pedasos_students",
-        verbose_name=_("Pedasos: Students"),
-        extra_args={"has_header_row": True, "separator": "\t"},
-        fields=[
-            UniqueReferenceFieldType,
-            LastNameFieldType,
-            FirstNameFieldType,
-            DateOfBirthFieldType,
-            SexFieldType,
-            PrimaryGroupByShortNameFieldType,
-            IgnoreFieldType,  # MOTHER
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # FATHER
-            IgnoreFieldType,
-            IgnoreFieldType,
-            GroupMembershipByShortNameFieldType,  # Course 1
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,  # Course 5
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,  # Course 10
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,  # Course 15
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,
-            GroupMembershipByShortNameFieldType,  # Course 20
-            GroupMembershipByShortNameFieldType,  # Course 21
-        ],
-    )
-    update_or_create_template(
-        Person,
-        name="pedasos_guardians_1",
-        verbose_name=_("Pedasos: Guardians 1"),
-        extra_args={"has_header_row": True, "separator": "\t"},
-        fields=[
-            ChildByUniqueReference,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            LastNameFieldType,  # MOTHER
-            FirstNameFieldType,
-            EmailFieldType,
-            IgnoreFieldType,  # FATHER
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # Course 1
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # Course 5
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # Course 10
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # Course 15
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # Course 20
-            IgnoreFieldType,  # Course 21
-        ],
-    )
-    update_or_create_template(
-        Person,
-        name="pedasos_guardians_2",
-        verbose_name=_("Pedasos: Guardians 2"),
-        extra_args={"has_header_row": True, "separator": "\t"},
-        fields=[
-            ChildByUniqueReference,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # MOTHER
-            IgnoreFieldType,
-            IgnoreFieldType,
-            LastNameFieldType,  # FATHER
-            FirstNameFieldType,
-            EmailFieldType,
-            IgnoreFieldType,  # Course 1
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # Course 5
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # Course 10
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # Course 15
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,
-            IgnoreFieldType,  # Course 20
-            IgnoreFieldType,  # Course 21
-        ],
-    )
+    template_defs = toml.load(os.path.join(od.path.dirname(__file__), "default_templates.toml"))
+
+    for name, defs in template_defs.items():
+        model = apps.get_model(defs["model"])
+        fields = [field_type_registry.get_from_name(field_type) for field_type in defs["fields"]]
+
+        update_or_create_template(
+            model,
+            name=defs["name"],
+            verbose_name=defs.get("verbose_name", ""),
+            extra_args=defs.get("extra_args", {}),
+            fields
+        )
