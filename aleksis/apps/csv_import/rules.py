86c91f102eb86e45b35b5e8efcf89a13c3466476
Hangzhi Yu <hangzhi@protonmail.com>
2021-05-19 17:46:35 +0000

    Rename rule

--- aleksis/apps/csv_import/rules.py
+++ aleksis/apps/csv_import/rules.py
@@ -3,4 +3,4 @@ from rules import add_perm
 from aleksis.core.util.predicates import has_global_perm, has_person
 
 import_data_predicate = has_person & has_global_perm("csv_import.import_data")
-add_perm("csv_import.import_data", import_data_predicate)
+add_perm("csv_import.import_data_rule", import_data_predicate)
